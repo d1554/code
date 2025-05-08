528bb02fcbe83dd1e23aea52df4f1ba3f07bcf7d
d1554 <czc2004211@gmail.com>
2025-05-08 16:37:46 +0800

    更新 emby-hide.js
    第二

--- emby-hide.js
+++ emby-hide.js
@@ -1,4 +1,13 @@
-// emby-hide.js
+// ==UserScript==
+// @name         Emby Header Cleaner
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Remove specific headers from Emby requests
+// @author       Your Name
+// @match        *://www.qdz1.top/emby/Users/AuthenticateByName*
+// @grant        none
+// ==/UserScript==
+
 let headers = $request.headers;
 
 // 删除敏感头字段
