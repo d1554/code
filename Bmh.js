78b6f528180b87afcff41786da3b419d45b863a2
d1554 <czc2004211@gmail.com>
2025-05-08 22:41:16 +0800

    创建 Bmh.js
    2

---
+++ Bmh.js
@@ -0,0 +1,4 @@
+[Rewrite]
+^https:\/\/www\.qdz1\.top\/ url request-header (\r\n)User-Agent:.+?(\r\n) request-header $1User-Agent: SenPlayer/5.5.0 (iPhone; iOS 18.3.2; Scale/3.00)$2
+[MITM]
+hostname = *.qdz1.top,
