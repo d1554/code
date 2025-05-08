c1ca4bbebf5b0b4d0f4ea219bfd685360b23d897
d1554 <czc2004211@gmail.com>
2025-05-08 21:43:19 +0800

    更新 Bao.js
    3

--- Bao.js
+++ Bao.js
@@ -4,7 +4,5 @@ let headers = $request.headers;
 // 修改 X-Emby-Client 为 SenPlayer
 headers['X-Emby-Client'] = 'SenPlayer';
 
-// 修改 User-Agent 为指定值
-^https:\/\/www\.qdz1\.top\/ url request-header (\r\n)User-Agent:.+?(\r\n) request-header $1User-Agent: SenPlayer/5.5.0 (iPhone; iOS 18.3.2; Scale/3.00)$2
 
 $done({ headers });
