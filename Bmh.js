0979b49c9db5a05af028f840d322e4df3a42a477
d1554 <czc2004211@gmail.com>
2025-05-08 22:46:21 +0800

    更新 Bmh.js
    3

--- Bmh.js
+++ Bmh.js
@@ -1,4 +1,5 @@
-[Rewrite_local]
+[rewrite_local]
 ^https:\/\/www\.qdz1\.top\/ url request-header (\r\n)User-Agent:.+?(\r\n) request-header $1User-Agent: SenPlayer/5.5.0 (iPhone; iOS 18.3.2; Scale/3.00)$2
-[MITM]
+
+[mitm]
 hostname = *.qdz1.top
